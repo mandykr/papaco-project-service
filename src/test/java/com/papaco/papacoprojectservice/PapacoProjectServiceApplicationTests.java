f7c0a67250c3fcd6b01ddf2ce3648034d7836594
mandykr <hongseokii92@gmail.com>
2022-11-07 14:09:42 +0900

    Initial commit

---
+++ src/test/java/com/papaco/papacoprojectservice/PapacoProjectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papaco.papacoprojectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapacoProjectServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
